16989e226f3900a9ae17189449dd37fb5d9883ee
prPedr <prpedro025@gmail.com>
2026-01-14 05:19:10 -0300

    Conversor de fareraid para celsius

---
+++ src/Temperatura.java
@@ -0,0 +1,9 @@
+public class Temperatura {
+    public static void main(String[] args) {
+        double f = 100;
+
+        double c = 5.0 / 9.0 * (f - 32);
+
+        System.out.printf("%.2f graus fireraid é igual a %.2f graus celsius", f, c);
+    }
+}
