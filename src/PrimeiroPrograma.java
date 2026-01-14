0e003850fffa267a41f01c32526f1d12dc65b81d
prPedr <prpedro025@gmail.com>
2026-01-14 04:21:14 -0300

    Testando comentarios

--- src/PrimeiroPrograma.java
+++ src/PrimeiroPrograma.java
@@ -1,5 +1,14 @@
 public class PrimeiroPrograma {
     public static void main(String[] args) {
+        // Comentario de uma linha
         System.out.println("Hello World");
+
+        /*
+            Comentarios de multiplas linhas
+            Linha de teste
+            Outra linha de teste
+        */
+
+        System.out.println("Texto apos uso de comentario");
     }
 }
