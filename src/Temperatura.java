011a9fc110a49506aaef8af6aec1744116839310
prPedr <prpedro025@gmail.com>
2026-01-14 05:22:41 -0300

    Adicionando o final para a calculo nao sofrer alteracao e adiconando um comentario sobre a formula

--- src/Temperatura.java
+++ src/Temperatura.java
@@ -2,7 +2,8 @@ public class Temperatura {
     public static void main(String[] args) {
         double f = 100;
 
-        double c = 5.0 / 9.0 * (f - 32);
+        // Formula para coversao de fireraid para celsius
+        final double c = 5.0 / 9.0 * (f - 32);
 
         System.out.printf("%.2f graus fireraid é igual a %.2f graus celsius", f, c);
     }
