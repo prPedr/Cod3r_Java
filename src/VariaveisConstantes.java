8cde7efc12a4672f0671fc062a4e8460ecd2eb08
prPedr <prpedro025@gmail.com>
2026-01-14 04:56:19 -0300

    Segundo commit sobre variaveis e constantes

--- src/VariaveisConstantes.java
+++ src/VariaveisConstantes.java
@@ -1,2 +1,12 @@
 public class VariaveisConstantes {
+    public static void main(String[] args) {
+        int num1 = 10;
+
+        // Uma vez que um valor possui "final" ele nao pode ser mudado dentro do algoritimo
+        final int num2 = 5;
+
+        int resultado = num1 + num2;
+
+        System.out.println(resultado);
+    }
 }
